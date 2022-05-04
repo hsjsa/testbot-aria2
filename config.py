dc585981953f8beb1d1121fd7d1a0d6e795e3483
hsjsa <101786584+hsjsa@users.noreply.github.com>
2022-05-04 23:23:51 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config(object):
+	"""docstring for Config"""
+	G_DRIVE_CLIENT_ID = "39122843696-fhq1q0bk2c9hvkc5r0n56nr7nmdeer3h.apps.googleusercontent.com"
+	G_DRIVE_CLIENT_SECRET = "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"
+	GDRIVE_FOLDER_ID = "1faNz4IHazQrehNa8Fb9bgJiQS43H861K"
